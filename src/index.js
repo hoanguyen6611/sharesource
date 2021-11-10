f7b020b3b42c319d7d1134e790907eebfb5063f8
hoane <19119179@student.hcmute.edu.vn>
2021-11-10 11:47:07 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const express = require('express');
-// const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT||3000;
 const route = require('./routes');
 const db = require('./config/db');
 const methodOverride = require('method-override');
@@ -11,12 +10,10 @@ const express_handlebars_sections = require('express-handlebars-sections');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const userController = require('./app/controllers/UserController');
-// const SortMiddleware = require('./app/middlewares/SortMiddleware');
 //Connect to DB server
 db.connect();
 app.use(express.static(path.join(__dirname, 'public')));
 // HTTP logger
-// app.use(morgan('combined'));
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
     secret: 'keyboard cat',
@@ -36,7 +33,6 @@ app.use(
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.json());
-// require('./app/middlewares/LocalMiddleware')(app);
 app.use(async function (req, res, next) {
     if (req.session.isAuthenticated === null) {
         req.session.isAuthenticated = false;
@@ -56,12 +52,11 @@ app.engine(
         },
     }),
 );
-// app.use(SortMiddleware);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 // route init
 route(app);
 
-app.listen(process.env.PORT, function(){
+app.listen(port, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
