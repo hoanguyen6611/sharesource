ffd70e7dd26b0f0458baacd7621e963c25aeb360
hoang <hoahuy2606@gmail.com>
2021-11-21 11:07:36 +0700

    update delete of

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = process.env.PORT||'3000';
+const port = process.env.PORT||3000;
 const route = require('./routes');
 const db = require('./config/db');
 const methodOverride = require('method-override');
 const express_handlebars_sections = require('express-handlebars-sections');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const userController = require('./app/controllers/UserController');
 //Connect to DB server
 db.connect();
 app.use(express.static(path.join(__dirname, 'public')));
 // HTTP logger
-app.set('trust proxy', 1) // trust first proxy
+app.set('trust proxy', 1);
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
@@ -30,7 +28,6 @@ app.use(
         extended: true,
     }),
 );
-app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(async function (req, res, next) {
